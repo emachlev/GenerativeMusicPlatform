7a39faf35aa5c49944e6b60ded85f33a5eb62cfc
Eran Machlev <eran.machlev@gmail.com>
2019-05-24 17:39:44 +0300

    Names and doc

--- static/js/pieces/aisatsana.js
+++ static/js/pieces/aisatsana.js
@@ -1,27 +1,27 @@
 BPM = 204;  // The original song is at 102 BPM, but some notes are played on a half-beat interval, which is why we double it and count half-beats as regular beats
 SECONDS_PER_MINUTE = 60;
-EIGHTH_NOTES_IN_BEAT = 2;
-EIGHTH_NOTE_INTERVAL_SECONDS = SECONDS_PER_MINUTE / (EIGHTH_NOTES_IN_BEAT * BPM);  // Defines how long a single note should be played
+NOTES_IN_BEAT = 2;
+NOTE_INTERVAL_SECONDS = SECONDS_PER_MINUTE / (NOTES_IN_BEAT * BPM);  // Defines how long a single note should be played
 SONG_LENGTH = 301;  // Used to iterate the MIDI JSON
 
 
 $.getJSON('/static/midi/aisatsana.json', function (data) {
 
     notes = data.tracks[1].notes.slice(0);  // The entire track
-    eigthNotes = [];  // Eigth notes that are pressed at each beat
+    pressedNotes = [];  // Notes that are pressed at each beat
 
-    for (time = 0; time <= SONG_LENGTH; time += EIGHTH_NOTE_INTERVAL_SECONDS) {  // Filling the list
-        pressedNoteNames = notes.filter(note =>
-            time <= note.time && note.time < time + EIGHTH_NOTE_INTERVAL_SECONDS
+    for (time = 0; time <= SONG_LENGTH; time += NOTE_INTERVAL_SECONDS) {  // Filling the list
+        pressedNotesInCurrentBeat = notes.filter(note =>
+            time <= note.time && note.time < time + NOTE_INTERVAL_SECONDS
         ).map(({name}) => name).sort();
-        eigthNotes.push(pressedNoteNames.join(','));
+        pressedNotes.push(pressedNotesInCurrentBeat.join(','));
     }
 
     phrases = [];  // List of phrases
     phraseLengthBeats = 32;  // Every phrase is 16 beats
-    eigthNotesCopy = eigthNotes.slice(0);
-    while (eigthNotesCopy.length > 0) {  // Divide pressed eigth notes to phrases
-        phrases.push(eigthNotesCopy.splice(0, phraseLengthBeats));
+    pressedNotesCopy = pressedNotes.slice(0);
+    while (pressedNotesCopy.length > 0) {  // Divide pressed eigth notes to phrases
+        phrases.push(pressedNotesCopy.splice(0, phraseLengthBeats));
     }
 
     phrasesWithIndex = phrases.map(phrase =>
@@ -31,26 +31,26 @@ $.getJSON('/static/midi/aisatsana.json', function (data) {
     );
 
     chain = new Chain(phrasesWithIndex);
+    console.log(phrases);
 
 
     Tone.Transport.scheduleRepeat(
         schedule,
-        phraseLengthBeats * EIGHTH_NOTE_INTERVAL_SECONDS
+        phraseLengthBeats * NOTE_INTERVAL_SECONDS
     );
 });
 
 
 schedule = () => {  // For each generated phrase (runs indefinitely)
-    phrase = []
+    phrase = [];
     while (phrase.filter(ph=> ph.includes(',')).length < 5) {  // To make the phrases longer and avoid empty phrases
         phrase = chain.walk()  // Walk the markov chain and get a phrase
     }
-    console.log(phrase);
     phrase.forEach(str => {  // For each beat in the phrase
         [t, ...names] = str.split(',');  // returns [index, note1, note2...] or just [index] if current beat is a rest
         parsedT = Number.parseInt(t, 10);  // Get the current beat's delay in the phrase
         names.forEach(name => {  // Play every beat in the specified delay (index*duration_of_eigth_note)
-            waitTime = parsedT * EIGHTH_NOTE_INTERVAL_SECONDS;
+            waitTime = parsedT * NOTE_INTERVAL_SECONDS;
             piano.triggerAttack(name, `+${waitTime + 1}`);
         });
     });
